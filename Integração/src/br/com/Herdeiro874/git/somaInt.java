dfc63000db44e25d0816e157bf4e6fa071bf1055
Herdeiro874 <113552298+Herdeiro874@users.noreply.github.com>
2023-03-02 12:06:50 -0300

    Upload dos arquivos iniciais

---
+++ Integração/src/br/com/Herdeiro874/git/somaInt.java
@@ -0,0 +1,23 @@
+package br.com.Herdeiro874.git;
+
+public class somaInt {
+	
+
+	public static void main(String[] args)
+	{
+		
+		int x, y, z;
+		
+		System.out.print("escreve x\n ");
+		x = MyIO.readInt();
+		System.out.print("escreve y\n ");
+		y = MyIO.readInt();
+		
+		z = x + y;
+		
+		System.out.print("x + y = " + z);
+	
+	}
+	
+	
+}
---
+++ Integração/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author felli
+ *
+ */
+module Integração {
+}
